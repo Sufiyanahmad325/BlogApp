2b98cb34f6995458c13a822f8c6950b4c9cfd2c0
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-12-03 11:40:13 +0530

    ActivityIndicator has been created

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,15 +2,45 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { store } from '../ReduxToolkit/store';
 import { CookiesProvider } from 'react-cookie';
+import { ActivityIndicator, View } from 'react-native';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
+
+
+function GlobalLoaderWrapper({ children }) {
+  const isLoading = useSelector(state => state.blogSlice.isLoading);
+
+  return (
+    <>
+      {children}
+
+      {isLoading && (
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(0,0,0,0.4)",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 9999,
+          }}
+        >
+          <ActivityIndicator size="large" color="#fff" />
+        </View>
+      )}
+    </>
+  );
+}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -19,6 +49,7 @@ export default function RootLayout() {
     <CookiesProvider >
     <Provider store={store}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <GlobalLoaderWrapper>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="screen/myblogsScreen" options={{ headerShown: false }} />
@@ -29,6 +60,7 @@ export default function RootLayout() {
           <Stack.Screen name="screen/signUp" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
         </Stack>
+        </GlobalLoaderWrapper>
         <StatusBar style="auto" />
       </ThemeProvider>
     </Provider>
